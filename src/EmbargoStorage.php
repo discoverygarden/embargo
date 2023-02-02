7be4e98313ac86252567fa350e126a86ef5aa63e
Akanksha Singh <akanksha@Akankshas-MacBook-Pro-2.local>
2023-02-02 17:09:49 +0530

    ARQT-16: Make embargo work with IHA

--- src/EmbargoStorage.php
+++ src/EmbargoStorage.php
@@ -2,22 +2,27 @@
 
 namespace Drupal\embargo;
 
+use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Cache\MemoryCache\MemoryCacheInterface;
 use Drupal\Core\Database\Connection;
 use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\Sql\SqlContentEntityStorage;
+use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\file\FileInterface;
+use Drupal\islandora_hierarchical_access\LUTHelper;
 use Drupal\media\MediaInterface;
 use Drupal\node\NodeInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
+use function PHPUnit\Framework\assertInstanceOf;
 
 /**
  * Storage for embargo entities.
@@ -32,19 +37,28 @@ class EmbargoStorage extends SqlContentEntityStorage implements EmbargoStorageIn
   protected $request;
 
   /**
-   * Th current user.
+   * The current user.
    *
    * @var \Drupal\Core\Session\AccountInterface
    */
   protected $user;
 
   /**
+   * The lookup table helper.
+   *
+   * @var \Drupal\islandora_hierarchical_access\LUTHelper $lutHelper
+   */
+  protected $lutHelper;
+
+
+  /**
    * Constructor.
    */
-  public function __construct(EntityTypeInterface $entity_type, Connection $database, EntityFieldManagerInterface $entity_field_manager, CacheBackendInterface $cache, LanguageManagerInterface $language_manager, MemoryCacheInterface $memory_cache, EntityTypeBundleInfoInterface $entity_type_bundle_info, EntityTypeManagerInterface $entity_type_manager, RequestStack $request_stack, AccountInterface $user) {
+  public function __construct(EntityTypeInterface $entity_type, Connection $database, EntityFieldManagerInterface $entity_field_manager, CacheBackendInterface $cache, LanguageManagerInterface $language_manager, MemoryCacheInterface $memory_cache, EntityTypeBundleInfoInterface $entity_type_bundle_info, EntityTypeManagerInterface $entity_type_manager, RequestStack $request_stack, AccountInterface $user, LUTHelper $lutHelper) {
     parent::__construct($entity_type, $database, $entity_field_manager, $cache, $language_manager, $memory_cache, $entity_type_bundle_info, $entity_type_manager);
     $this->request = $request_stack->getCurrentRequest();
     $this->user = $user;
+    $this->lutHelper = $lutHelper;
   }
 
   /**
@@ -52,17 +66,18 @@ class EmbargoStorage extends SqlContentEntityStorage implements EmbargoStorageIn
    */
   public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
     return new static(
-          $entity_type,
-          $container->get('database'),
-          $container->get('entity_field.manager'),
-          $container->get('cache.entity'),
-          $container->get('language_manager'),
-          $container->get('entity.memory_cache'),
-          $container->get('entity_type.bundle.info'),
-          $container->get('entity_type.manager'),
-          $container->get('request_stack'),
-          $container->get('current_user'),
-      );
+      $entity_type,
+      $container->get('database'),
+      $container->get('entity_field.manager'),
+      $container->get('cache.entity'),
+      $container->get('language_manager'),
+      $container->get('entity.memory_cache'),
+      $container->get('entity_type.bundle.info'),
+      $container->get('entity_type.manager'),
+      $container->get('request_stack'),
+      $container->get('current_user'),
+      $container->get('islandora_hierarchical_access.lut_helper'),
+    );
   }
 
   /**
@@ -78,18 +93,19 @@ class EmbargoStorage extends SqlContentEntityStorage implements EmbargoStorageIn
 
   /**
    * {@inheritdoc}
+   * @throws \Exception
    */
   public function getApplicableEmbargoes(EntityInterface $entity): array {
     if ($entity instanceof NodeInterface) {
       $properties = ['embargoed_node' => $entity->id()];
       return $this->loadByProperties($properties);
     }
-    elseif ($entity instanceof MediaInterface) {
-      // If a media entity has any field that relates to a node we check that
+    elseif ($entity instanceof MediaInterface || $entity instanceof FileInterface) {
+      // If a media or file entity has any field that relates to a node we check that
       // node for applicable embargoes.
       $applicable = [];
-      $isHelper = \Drupal::service('islandora_hierarchical_access.lut_helper');
-      $referencedNodeIds = $isHelper->lookUpFields('nid', ['field' => 'mid', 'value' => $entity->id()]);
+      $conditionFieldName = $entity instanceof MediaInterface ? 'mid' : 'fid';
+      $referencedNodeIds = $this->lutHelper->lookUpFields('nid', ['field' => $conditionFieldName, 'value' => $entity->id()]);
       $referencedNodes = $this->entityTypeManager->getStorage('node')->loadMultiple($referencedNodeIds);
       foreach ($referencedNodes as $node) {
         $applicable = array_merge($applicable, $this->getApplicableEmbargoes($node));
@@ -97,15 +113,7 @@ class EmbargoStorage extends SqlContentEntityStorage implements EmbargoStorageIn
 
       return array_unique($applicable, SORT_REGULAR);
     }
-    elseif ($entity instanceof FileInterface) {
-      $applicable = [];
-      $isHelper = \Drupal::service('islandora_hierarchical_access.lut_helper');
-      $referencedNodes = $isHelper->lookUpFields('nid', ['field' => 'fid', 'value' => $entity->id()]);
-      foreach ($referencedNodes as $node) {
-        $applicable = array_merge($applicable, $this->getApplicableEmbargoes($node));
-      }
-      return array_unique($applicable, SORT_REGULAR);
-    }
+
     return [];
   }
 
@@ -117,14 +125,14 @@ class EmbargoStorage extends SqlContentEntityStorage implements EmbargoStorageIn
     $user = $user ?? $this->user;
     $ip = $ip ?? $this->request->getClientIp();
     return array_filter(
-          $this->getApplicableEmbargoes($entity), function ($embargo) use ($entity, $timestamp, $user, $ip): bool {
-              $inactive = $embargo->expiresBefore($timestamp);
-              $type_exempt = ($entity instanceof NodeInterface && $embargo->getEmbargoType() !== EmbargoInterface::EMBARGO_TYPE_NODE);
-              $user_exempt = $embargo->isUserExempt($user);
-              $ip_exempt = $embargo->ipIsExempt($ip);
-              return !($inactive || $type_exempt || $user_exempt || $ip_exempt);
-          }
-      );
+      $this->getApplicableEmbargoes($entity), function ($embargo) use ($entity, $timestamp, $user, $ip): bool {
+      $inactive = $embargo->expiresBefore($timestamp);
+      $type_exempt = ($entity instanceof NodeInterface && $embargo->getEmbargoType() !== EmbargoInterface::EMBARGO_TYPE_NODE);
+      $user_exempt = $embargo->isUserExempt($user);
+      $ip_exempt = $embargo->ipIsExempt($ip);
+      return !($inactive || $type_exempt || $user_exempt || $ip_exempt);
+    }
+    );
   }
 
 }
