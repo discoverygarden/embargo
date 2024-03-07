e087fdf1149f537a23e468102b79bde4efbfaf89
Akanksha <akankshasingh.viet@gmail.com>
2024-03-07 19:44:53 +0530

    Fix typo

--- tests/src/Kernel/EmbargoAccessQueryTaggingAlterTest.php
+++ tests/src/Kernel/EmbargoAccessQueryTaggingAlterTest.php
@@ -64,7 +64,7 @@ class EmbargoAccessQueryTaggingAlterTest extends EmbargoKernelTestBase {
   public function testNodeEmbargoReferencedFileAccessQueryAlterAccessDenied() {
     $query = $this->generateFileSelectAccessQuery($this->user, 'view');
     $result = $query->execute()->fetchAll();
-    $this->assertCount(1, $result, json_encode($result));
+    $this->assertCount(1, $result, 'File of embargoed nodes cannot be viewed');
   }
 
   /**
