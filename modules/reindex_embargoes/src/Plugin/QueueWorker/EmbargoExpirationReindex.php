49300aeaf5eb642c77501378e52a9e4135903886
Chris MacDonald <31731869+chrismacdonaldw@users.noreply.github.com>
2025-05-12 16:09:10 -0300

    Refactor to throw DelayedRequeueException, also be slightly more testable.

--- modules/reindex_embargoes/src/Plugin/QueueWorker/EmbargoExpirationReindex.php
+++ modules/reindex_embargoes/src/Plugin/QueueWorker/EmbargoExpirationReindex.php
@@ -6,8 +6,9 @@ use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Queue\QueueFactory;
+use Drupal\Core\Queue\DelayedRequeueException;
 use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\search_api\IndexInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -36,13 +37,6 @@ class EmbargoExpirationReindex extends QueueWorkerBase implements ContainerFacto
   protected TimeInterface $time;
 
   /**
-   * The queue factory.
-   *
-   * @var \Drupal\Core\Queue\QueueFactory
-   */
-  protected QueueFactory $queueFactory;
-
-  /**
    * The configuration factory.
    *
    * @var \Drupal\Core\Config\ConfigFactoryInterface
@@ -50,51 +44,82 @@ class EmbargoExpirationReindex extends QueueWorkerBase implements ContainerFacto
   protected ConfigFactoryInterface $configFactory;
 
   /**
+   * Constructor.
+   */
+  public function __construct(
+    array $configuration,
+          $plugin_id,
+          $plugin_definition,
+    EntityTypeManagerInterface $entity_type_manager,
+    TimeInterface $time,
+    ConfigFactoryInterface $config_factory
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+    $this->time = $time;
+    $this->configFactory = $config_factory;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    $instance = new static($configuration, $plugin_id, $plugin_definition);
-
-    $instance->entityTypeManager = $container->get('entity_type.manager');
-    $instance->time = $container->get('datetime.time');
-    $instance->queueFactory = $container->get('queue');
-    $instance->configFactory = $container->get('config.factory');
-    return $instance;
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('datetime.time'),
+      $container->get('config.factory')
+    );
   }
 
   /**
    * {@inheritdoc}
    */
   public function processItem($data): void {
+    if (!isset($data['embargo_id'])) {
+      return;
+    }
+
     $embargo = $this->entityTypeManager->getStorage('embargo')->load($data['embargo_id']);
     if (!$embargo) {
       return;
     }
 
-    $current_time = $this->time->getRequestTime();
-    $current_expiration = $embargo->getExpirationDate()->getTimestamp();
-
-    if ($current_expiration > $current_time + 86400) {
+    $expiration_date_object = $embargo->getExpirationDate();
+    if (!$expiration_date_object || !$expiration_date_object->getTimestamp()) {
       return;
     }
 
-    if ($current_expiration > $current_time && $current_expiration < $current_time + 86400) {
-      $this->queueFactory->get('embargo_expiration_reindex')->createItem($data);
-      return;
+    $expiration_timestamp = $expiration_date_object->getTimestamp();
+    $current_timestamp = $this->time->getRequestTime();
+
+    // If the expiration time is still in the future, requeue it with a delay.
+    if ($expiration_timestamp > $current_timestamp) {
+      $delay = $expiration_timestamp - $current_timestamp;
+      throw new DelayedRequeueException($delay, "Embargo ID {$embargo->id()} not yet expired. Requeuing.");
     }
 
     $embargoed_node = $embargo->getEmbargoedNode();
     if ($embargoed_node) {
       $config = $this->configFactory->get('reindex_embargoes.settings');
       $selected_indexes = array_values($config->get('selected_indexes') ?? []);
+
       foreach ($selected_indexes as $index_id) {
         $index = $this->entityTypeManager->getStorage('search_api_index')->load($index_id);
-        if ($index && $index->isServerEnabled() && $index->getDatasource('entity:node')) {
-          $doc_ids = [];
-          foreach ($embargoed_node->getTranslationLanguages() as $language) {
-            $doc_ids[] = $embargoed_node->id() . ':' . $language->getId();
+        if ($index instanceof IndexInterface && $index->isServerEnabled() && $index->status()) {
+          if ($index->isValidDatasource('entity:node')) {
+            $node_ids_to_reindex = [];
+            // Item ID format is typically 'entity_id:langcode'.
+            foreach ($embargoed_node->getTranslationLanguages() as $language) {
+              $node_ids_to_reindex[] = $embargoed_node->id() . ':' . $language->getId();
+            }
+
+            if (!empty($node_ids_to_reindex)) {
+              $index->trackItemsUpdated('entity:node', $node_ids_to_reindex);
+            }
           }
-          $index->trackItemsUpdated('entity:node', $doc_ids);
         }
       }
     }
